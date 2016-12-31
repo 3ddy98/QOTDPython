7df5012f93b893e036fb8641cc4bcd2e40a6a4c7
Edward Ponce <eponc006@ucr.edu>
2016-12-30 18:50:10 -0800

    Added Scheduler
    The scheduler runs the spam script every day at 6:31 p.m.

--- QOTDPython/QOTD.py
+++ QOTDPython/QOTD.py
@@ -1,38 +1,47 @@
 import random
+import logging
 import smtplib
+from apscheduler.schedulers.blocking import BlockingScheduler
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
-list = ["emails"]
-for emails in list:
-	fromaddr="SenderAddress"
-	toaddr= emails
-	msg=MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To']= toaddr
-	msg['Subject'] = "Quote of the Day"
+list = ['eaponce.vanguard@gmail.com']
+def qsender():
+	for emails in list:
+		fromaddr="eaponce.vanguard@gmail.com"
+		toaddr= emails
+		msg=MIMEMultipart()
+		msg['From'] = fromaddr
+		msg['To']= toaddr
+		msg['Subject'] = "Quote of the Day"
 	
 
-	filename = "troll.png"	
-	attachment = open("troll.png","rb")
+		filename = "troll.png"	
+		attachment = open("troll.png","rb")
 
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= {}".format(filename))
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= {}".format(filename))
 	
-	lines = open('Quotes.txt').read().splitlines()
-	quote = random.choice(lines)
-	msg.attach(part)
+		lines = open('Quotes.txt').read().splitlines()
+		quote = random.choice(lines)
+		msg.attach(part)
 	
-	body = "Someone somewhere once said...\n{}".format(quote)
-	msg.attach(MIMEText(body, 'plain'))
+		body = "Someone somewhere once said...\n{}".format(quote)
+		msg.attach(MIMEText(body, 'plain'))
 
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login(fromaddr,"Password")
-	text = msg.as_string()
-	server.sendmail(fromaddr, toaddr, text)
-	server.quit()
- 
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.starttls()
+		server.login(fromaddr,"Password")
+		text = msg.as_string()
+		server.sendmail(fromaddr, toaddr, text)
+		server.quit()
+
+#The scheduler starts here
+sched = BlockingScheduler()
+
+sched.add_job(qsender, 'cron', hour ='18', minute ='31')
+logging.basicConfig()
+sched.start()
